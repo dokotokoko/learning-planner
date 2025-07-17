9d0ee2feb90bb875f5b93929119333c9e39d10d3
Ko Takado <koutakado9@gmail.com>
2025-07-17 15:07:16 +0900

    ローカルのURLを変更

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://localhost:3000",
+        "http://127.0.0.1:8080",
+        "http://localhost:8080",
         os.environ.get("ENGROK_ENDPOINT")
     ],
     allow_credentials=True,
