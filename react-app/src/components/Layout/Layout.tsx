1f549ed3dc1919c0cebb1340c2604ccc66180f9d
HiRoMu16 <hiro.mori.98@gmail.com>
2025-09-04 01:48:05 +0900

    mori検証画面表示

--- react-app/src/components/Layout/Layout.tsx
+++ react-app/src/components/Layout/Layout.tsx
@@ -267,8 +267,8 @@ const Layout: React.FC = () => {
   const mainListItems: MenuItem[] = useMemo(() => [
     { text: 'ダッシュボード', icon: <TipsAndUpdates />, path: '/dashboard' },
     { text: '探究テーマを見つける・探す', icon: <Explore />, path: '/framework-games/theme-deep-dive' },
-    // { text: '対話エージェント検証', icon: <Psychology />, path: '/conversation-agent-test' },
-    // { text: '探究クエスト掲示板!', icon: <Explore />, path: '/quests'} // 一時的に非表示
+    { text: '対話エージェント検証', icon: <Psychology />, path: '/conversation-agent-test' },
+    { text: '探究クエスト掲示板!', icon: <Explore />, path: '/quests'} // 一時的に非表示
   ], []);
 
   // 展開状態のサイドバー
